47d4c8d9a4db83391459b61925d85a57b0c7503b
Lucas's Laptop <aholucascode@gmail.com>
2024-08-18 11:23:03 -0600

    robots.txt make worky

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://lucas-asher.com",
-    integrations: [sitemap()]
+    integrations: [sitemap(), robotsTxt()]
 });
